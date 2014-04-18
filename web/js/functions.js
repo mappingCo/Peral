b1100544193015c2e6bebc72bbe968048ea5561f
sigon <mapping.and.co@gmail.com>
2014-04-18 22:09:21 +0200

    add web page

---
+++ web/js/functions.js
@@ -0,0 +1,11 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+	// Background Image
+	 $.backstretch("../images/page-bg-img.jpg");
+
+});
